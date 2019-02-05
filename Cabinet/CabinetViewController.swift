ddc339d8f2c6912ed259fceacf5db9156f3074ae
Muukii <muukii.app@gmail.com>
2019-02-05 22:50:13 +0900

    WIP

--- Cabinet/CabinetViewController.swift
+++ Cabinet/CabinetViewController.swift
@@ -10,4 +10,32 @@ import Foundation
 
 open class CabinetViewController : UIViewController {
   
+  public let cabinetView: CabinetView
+  
+  public unowned let bodyViewController: UIViewController
+  
+  public init(_ bodyViewController: UIViewController) {
+    self.bodyViewController = bodyViewController
+    self.cabinetView = .init()
+    
+    super.init(nibName: nil, bundle: nil)
+    
+    addChild(bodyViewController)
+    
+  }
+  
+  @available(*, unavailable)
+  public required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  open override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.addSubview(cabinetView)
+    cabinetView.frame = view.bounds
+    cabinetView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    
+    
+  }
 }
