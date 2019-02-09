2406ede4fb7709f9a30665dcecb11f1a45ca07b0
Muukii <muukii.app@gmail.com>
2019-02-10 05:25:24 +0900

    Update

---
+++ Rideau/ScrollPanGesture.swift
@@ -0,0 +1,120 @@
+//
+//  ScrollPanGesture.swift
+//  Rideau
+//
+//  Created by muukii on 2019/02/10.
+//  Copyright © 2019 muukii. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIGestureRecognizerSubclass
+
+final class ScrollPanGestureRecognizer : UIPanGestureRecognizer {
+  
+  private weak var trackingScrollView: UIScrollView?
+  private var shouldKillScroll: Bool = false
+  
+  private var onceOperationWhenStartedTracking: () -> Void = {}
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+    trackingScrollView = event.findScrollView()
+    super.touchesBegan(touches, with: event)
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
+    
+    super.touchesMoved(touches, with: event)
+    
+    if let scrollView = trackingScrollView {
+      
+      guard
+        scrollView.contentOffset.y <= scrollView.contentInset.top
+        else {
+          shouldKillScroll = true
+          setTranslation(.zero, in: view)
+          return
+      }
+      
+      if shouldKillScroll {
+        shouldKillScroll = false
+//        scrollView.panGestureRecognizer.isEnabled = false
+//        scrollView.panGestureRecognizer.isEnabled = true
+        
+        var contentOffset = scrollView.contentOffset
+        contentOffset.y = scrollView.contentInset.top
+        if #available(iOS 11.0, *) {
+          contentOffset.y += scrollView.adjustedContentInset.top
+        }
+        scrollView.setContentOffset(contentOffset, animated: false)
+      }
+      
+    }
+    
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
+    super.touchesEnded(touches, with: event)
+  }
+  
+  override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
+    super.touchesCancelled(touches, with: event)
+  }
+}
+
+extension UIEvent {
+  
+  fileprivate func findScrollView() -> UIScrollView? {
+    
+    guard
+      let firstTouch = allTouches?.first,
+      let targetView = firstTouch.view
+      else { return nil }
+    
+    if let scrollView = targetView as? UIScrollView {
+      return scrollView
+    }
+    
+    let scrollView = ResponderChainIterator(responder: targetView)
+      .first { $0 is UIScrollView }
+
+    return (scrollView as? UIScrollView)
+  }
+  
+}
+
+#if DEBUG
+
+extension UIGestureRecognizer.State {
+  
+  fileprivate func localized() -> String {
+    
+    switch self {
+    case .possible: return "Possible"
+    case .began: return "Began"
+    case .changed: return "Changed"
+    case .ended: return "Ended"
+    case .cancelled: return "Cancelled"
+    case .failed: return "Failed"
+    }
+  }
+}
+
+#endif
+
+fileprivate final class ResponderChainIterator : IteratorProtocol, Sequence {
+  
+  public typealias Element = UIResponder
+  
+  private var currentResponder: UIResponder?
+  
+  public init(responder: UIResponder) {
+    currentResponder = responder
+  }
+  
+  public func next() -> UIResponder? {
+    
+    let next = currentResponder?.next
+    currentResponder = next
+    return next
+  }
+}
