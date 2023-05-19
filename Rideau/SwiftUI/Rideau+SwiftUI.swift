914d80ff9af74883a70986203ef5053fe5f48598
Muukii <muukii.app@gmail.com>
2023-05-20 02:39:33 +0900

    :zap: WIP

---
+++ Rideau/SwiftUI/Rideau+SwiftUI.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+#if DEBUG
+
+@available(iOS 14, *)
+enum Preview_Rideau: PreviewProvider {
+
+  static var previews: some View {
+
+    Group {
+      Text("")
+    }
+
+  }
+
+}
+
+#endif
