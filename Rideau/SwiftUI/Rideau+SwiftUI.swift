e7cf3a56de76dc61879c3b136c91a038f0af3c34
Muukii <muukii.app@gmail.com>
2023-06-02 01:34:44 +0900

    :zap: WIP

--- Rideau/SwiftUI/Rideau+SwiftUI.swift
+++ Rideau/SwiftUI/Rideau+SwiftUI.swift
@@ -25,15 +25,33 @@ struct SwiftUIRideau<Content: View>: UIViewControllerRepresentable {
 
     RideauViewController(
       bodyViewController: context.coordinator.hostingController,
-      configuration: .init(snapPoints: [.hidden, .fraction(1)], topMarginOption: .fromSafeArea(0)),
+      configuration: .init(
+        snapPoints: [.fraction(0.1), .fraction(1)],
+        topMarginOption: .fromSafeArea(0)
+      ),
       initialSnapPoint: .fraction(1),
       resizingOption: .noResize
     )
+
   }
 
   func updateUIViewController(_ uiViewController: RideauViewController, context: Context) {
     context.coordinator.hostingController.rootView = content
+
+    uiViewController.rideauView.move(to: .fraction(1), animated: true, completion: {})
   }
+
+}
+
+extension View {
+//
+//  public func rideau<Content: View>(
+//    isPresented: Binding<Bool>,
+//    onDismiss: (() -> Void)?,
+//    @ViewBuilder content: () -> Content
+//  ) -> some View {
+//
+//  }
 
 }
 
@@ -45,19 +63,37 @@ enum Preview_Rideau: PreviewProvider {
   static var previews: some View {
 
     Group {
-      ZStack {
-        Color.blue
-        GeometryReader { proxy in
-          let _ = print(proxy.size)
-          SwiftUIRideau(conetnt: {
-            Text("Hello")
-          })
-        }
-      }
+      ContentView()
     }
 
   }
 
+  struct ContentView: View {
+
+    @State var count: Int = 0
+
+    var body: some View {
+      ZStack {
+        Color.blue
+          .ignoresSafeArea()
+
+        SwiftUIRideau(conetnt: {
+          ZStack {
+            Color.gray
+
+            VStack {
+              Text("Hello \(count)")
+              Button("up") {
+                count += 1
+              }
+            }
+          }
+        })
+        .ignoresSafeArea()
+
+      }
+    }
+  }
 }
 
 #endif
