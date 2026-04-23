451fa4e4e3802faab910dfc69b81dbae855f622b
Muukii <muukii.app@gmail.com>
2026-04-24 00:58:37 +0900

    Update

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/FluidGroup/swiftui-scrollview-interoperable-drag-gesture",
-      revision: "6b74e353bbae6b8fbc5afe534c05769b6e4910c0"
+      from: "0.4.0"
     ),
     .package(url: "https://github.com/FluidGroup/swift-rubber-banding", from: "1.0.0"),
   ],
