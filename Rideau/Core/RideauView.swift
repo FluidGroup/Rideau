cc60da98dcc2912e9473822551ad4eaf9cbf2136
Muukii <muukii.app@gmail.com>
2026-07-21 22:03:59 +0900

    Update

--- Rideau/Core/RideauView.swift
+++ Rideau/Core/RideauView.swift
@@ -105,7 +105,7 @@ extension RideauView {
 /**
  A view that manages sheet UI.
 
- You may use ``RideauContentType`` in a view what you want to show in the sheet. 
+ You may use ``RideauContentType`` in a view what you want to show in the sheet.
  */
 public final class RideauView: RideauTouchThroughView {
 
@@ -296,7 +296,7 @@ public final class RideauView: RideauTouchThroughView {
       }
 
       let screenHeight = UIScreen.main.bounds.height
-      return screenHeight - v.cgRectValue.minY
+      return max(0, screenHeight - v.cgRectValue.minY)
     }
 
     var animationDuration: Double {
